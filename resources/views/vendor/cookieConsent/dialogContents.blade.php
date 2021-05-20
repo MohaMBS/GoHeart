c33be082c1ad2d51d4eb89538ffbc965235f33fb
MOHAMED BOUGHIMA <practicas.offing@gmail.com>
2021-05-20 16:59:16 +0200

    Cookie con borde

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,11 +1,11 @@
-<div class="col-12 js-cookie-consent cookie-consent bg-white text-center p-4 fixed-bottom">
+<div class="col-12 js-cookie-consent cookie-consent bg-white text-center p-2 fixed-bottom border-top border-dark">
 <div class="col-12">
     <span class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
     </span>
 </div>
 <div class="col-12">
-    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-success ">
+    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-success mt-2">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
 </div>
