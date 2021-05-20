104ac7a8a4e9e84d4684cabca6f0bc3733a636f1
MOHAMED BOUGHIMA <practicas.offing@gmail.com>
2021-05-20 16:52:57 +0200

    Incluido el tema de las cookies

---
+++ resources/lang/vendor/cookieConsent/es/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'Este sito web usa unas cookies personalizadas para el correcto funcionamiento de la web, no se almacena ningún tipo de cookie para fines terceros.',
+    'agree' => 'Aceptar cookie',
+];
---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,12 @@
+<div class="col-12 js-cookie-consent cookie-consent bg-white text-center p-4 fixed-bottom">
+<div class="col-12">
+    <span class="cookie-consent__message">
+        {!! trans('cookieConsent::texts.message') !!}
+    </span>
+</div>
+<div class="col-12">
+    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-success ">
+        {{ trans('cookieConsent::texts.agree') }}
+    </button>
+</div>
+</div>
