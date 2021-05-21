dc83028741c9cbee84eb916b4f8d78c6c65ea5dc
MOHAMED BOUGHIMA <practicas.offing@gmail.com>
2021-05-21 17:30:31 +0200

    Login de breeze cambiados de forma simple

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,8 +1,9 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            <a href="/" >
+                <img src="{{ asset('storage/web/images/logo.png')}}" width="75px" alt="logo web">
+                <h2 class="text-center"><b>Go</b>Heart <br> Login</h2>
             </a>
         </x-slot>
 
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            <a href="/" >
+                <img src="{{ asset('storage/web/images/logo.png')}}" width="75px" alt="logo web">
+                <h2 class="text-center"><b>Go</b>Heart <br> Login</h2>
             </a>
         </x-slot>
-
         <div class="mb-4 text-sm text-gray-600">
             {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
         </div>
