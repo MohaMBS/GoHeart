7b8b45788efca5e17d1bea3e0d0ea26c4ee02e18
MOHAMED BOUGHIMA <practicas.offing@gmail.com>
2021-05-25 17:38:41 +0200

    Sobre us

--- resources/views/goheart/about-us.blade.php
+++ resources/views/goheart/about-us.blade.php
@@ -14,18 +14,22 @@
                 <i style="font-size: 5rem;" class="fas fa-phone"></i>
             </div>
             <h4 class="title"><a href="{{route('form-contact')}}">Contactanos</a></h4>
-            <p>Tienes dudas sobre nuestra web, no dudes en darnos un toque y te ayudaremos.</p>
+            <p>Tienes dudas sobre nuestra la web, ¿no sabes que como funcciona? Pues no dudes, disponemos de un servicio de atencion a la comunidad excelente. Solo debes rellenar el formulario de contacto 
+                y un administrador se pondra en contacto contigo lo antes posible para poder ayudarte.
+            </p>
           </div>
         </div>
 
         <div class="col-12 d-flex align-items-stretch mb-5 mb-lg-4">
           <div class="col-12 bg-white p-3 rounded">
             <div class="text-center p-3">
-                <i style="font-size: 5rem;" class="fas fa-info-circle"></i>
-                <i style="font-size: 5rem;" class="fas fa-cookie"></i>
+                <i style="font-size: 5rem" class="fas fa-bullseye"></i>
             </div>
-            <h4 class="title"><a href="{{route('cookie')}}">Cookies y</a> <a href="{{route('privacy')}}">Politica de privacidad</a></h4>
-            <p>Aprende y lee sobre nuestra politica de privacidad y cookies tanto nuestars como de terceros.</p>
+            <h4 class="title"><a href="{{route('cookie')}}">Objetivos </a></h4>
+            <p>Nuestros objetivos, es que como comuidad ayudar a todo aquell que lo necesite en su dia a dia. 
+                Somos una comunidad bastante unida y con muchos usuario conectado a la vez, lo cuales te puede ayudar y guíar. 
+                No dudes en crear una entrada si queires aprender
+                 nuestro objetvio es que te cuides.</p>
           </div>
         </div>
 
@@ -35,21 +39,22 @@
                 <i style="font-size: 5rem;" class="fas fa-balance-scale"></i>
             </div>
             <h4 class="title"><a href="{{route('legal-warning')}}">Aviso legal</a></h4>
-            <p>Lee nuestro aviso legal y enterate de las leyes que te afectan.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+            <p>Te precupan tus datos personales, lee nuestro aviso legal y enterate de las leyes que te afectan. Nosotros somos una web qué vela por tu privacidad. Tu información no es compartida
+                con terceros, puede eliminar todos aquellos datos que tengamos registrados en la web.
+            </p>
             <p></p>
           </div>
         </div>
 
         <div class="col-12 d-flex align-items-stretch mb-5 mb-lg-4">
-          <div class="col-12 bg-white p-3 rounded">
-            <div class="text-center p-3">
-                <i style="font-size: 5rem;" class="fas fa-user-cog"></i>
+            <div class="col-12 bg-white p-3 rounded">
+                <div class="text-center p-3">
+                    <i style="font-size: 5rem;" class="fas fa-user-cog"></i>
+                </div>
+                <h4 class="title"><a href="{{route('update-profile')}}">Mi cuenta</a></h4>
+                <p>Siempre intentamos brindar la mejor seguridad para ti, por ese tenemos un sistema de gestión de datos bastante robusto con el cual puede modificar tu cuenta o hasta eliminarla de forma permanente y recuerda que tus datos serán borrados de forma automática en nuestra base de datos.</p>
             </div>
-            <h4 class="title"><a href="{{route('update-profile')}}">Modificar cuenta</a></h4>
-            <p>En este apartado puedes, modificar tus datos de usuario o eliminar tu cuenta.</p>
-          </div>
         </div>
-
       </div>
 
     </div>
